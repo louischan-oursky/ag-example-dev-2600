21fac81cdb16e26364caac49e713f7174c33c272
Louis Chan <louischan@oursky.com>
2025-04-15 18:10:54 +0800

    Document reset password via SMS OTP

---
+++ account-recovery-verify-code.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eux
+
+curl -s -XPOST \
+	-H "Content-Type: application/json" \
+	-d "
+{
+	\"state_token\": \"$STATE_TOKEN\",
+	\"input\": {
+		\"account_recovery_code\": \"$OTP\"
+	}
+}
+	" \
+	"$ENDPOINT/api/v1/authentication_flows/states/input"
